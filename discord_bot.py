fb195d716e2786c6906623f04b5d4531b2b796b3
Goochy12 <liamgooch@live.com.au>
2022-03-04 23:34:49 +1100

    functioning for 1 server

--- discord_bot.py
+++ discord_bot.py
@@ -2,24 +2,51 @@ import discord
 import asyncio
 
 import sneaker_code
-from sneaker_code import *
+
+sneakers = []
+changes = False
+channel = None
+
+raffle_message_true = "\U0001F911" + ' Raffles Live! ' + "\U0001F911"
+raffle_message_false = "No Raffles!"
+raffle_message_current = ""
 
 client = discord.Client()
 
-def getStatus():
-    if sneaker_code.checkForRaffles() is not []:
-        return "\U0001F911" + ' Raffles Live! ' + "\U0001F911"
-    return "No Raffles"
-
-async def status_task():
+async def check_sneakers():
+    global channel
     while True:
-        await client.change_presence(activity=discord.Game(name=getStatus()))
-        await asyncio.sleep(300)
+        print("LOOPING")
+        changes = sneaker_code.checkForChanges()
+
+        if changes["changes"] == True:
+            if channel != None:
+                await channel.send("New Sneakers Available")
+            pass
+        if changes["raffle"] == True:
+            raffle_message_current = raffle_message_true
+        else:
+            raffle_message_current = raffle_message_false
+
+        print(changes["changes"])
+        print(changes["raffle"])
+        print(raffle_message_current)
+
+        await client.change_presence(activity=discord.Game(name=raffle_message_current))
+
+        await asyncio.sleep(30)
+
+@client.event
+async def on_message(message):
+    global channel
+    if message.content.startswith('~setchannel'):
+        channel = message.channel
+        await channel.send("Default Channel changed to " + channel.name)
 
 @client.event
 async def on_ready():
     print("Logged on as {0.user}".format(client))
-    client.loop.create_task(status_task())
+    client.loop.create_task(check_sneakers())
 
 if __name__ == "__main__":
-    client.run('OTQ2NzQxNDEwNzYwMTA1OTk0.YhjHpQ.W_Y3LDawzeebNfERCk9xYoIr7iE')
+    client.run('OTQ2NzQxNDEwNzYwMTA1OTk0.YhjHpQ.P9dF620dtvXCT_-n_vxC671QkDM')
--- sneaker_code.py
+++ sneaker_code.py
@@ -14,19 +14,21 @@ def newCrawl(filename):
     os.system(f'scrapy crawl raffles -O {filename}.json')
 
 def getJSONFromFile(filename):
-    sneakers = {}
-    with open(filename) as json_file:
-        try:
+    sneakers = []
+    try:
+        with open(filename) as json_file:
             sneakers = json.load(json_file)
-        except:
-            sneakers = {}
+    except:
+        sneakers = []
     return sneakers
 
-def checkForRaffles():
-    oldSneakerJson = getJSONFromFile('sneakers.json')
-    raffles = ([d for d in oldSneakerJson if d['status'] == 'Enter Raffle'])
-    print(raffles)
+def checkForRaffles(jsonFile):
+    raffles = ([d for d in jsonFile if d['status'] == 'Enter Raffle'])
     return raffles
+
+def saveFile(file, filename):
+    with open(filename, 'w') as fout:
+        json.dump(file, fout)
 
 def checkForChanges():
     oldSneakerJson = getJSONFromFile('sneakers.json')
@@ -36,9 +38,25 @@ def checkForChanges():
     newCrawl('sneakers_new')
     newSneakerJson = getJSONFromFile('sneakers_new.json')
 
+    # print(newSneakerJson)
+    difference = [x for x in oldSneakerJson + newSneakerJson if x not in oldSneakerJson or x not in newSneakerJson]
+    print(len(oldSneakerJson))
+    print(len(newSneakerJson))
+    print(len(difference))
+
+    raffles = checkForRaffles(newSneakerJson)
+
+    changes = {"changes": False, "raffle": False}
+    if difference:
+        changes["changes"] = True
+    if raffles:
+        changes["raffle"] = True
+
+    saveFile(newSneakerJson, "sneakers.json")
     #check for changes here
     # old sneakers = new sneakers
-    return
+    return {"changes": True, "raffle": False}
+    return changes
 
 def startup():
     newCrawl('sneakers')
